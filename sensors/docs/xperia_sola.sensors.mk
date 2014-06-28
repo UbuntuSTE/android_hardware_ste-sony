705420ddc99e98d0bf72b94c9330d2711ad8308a
DevSwift <devswift@wp.pl>
2014-06-28 13:00:11 +0200

    sensors

---
+++ sensors/docs/xperia_sola.sensors.mk
@@ -0,0 +1,13 @@
+#
+# This is the DASH configuration makefile for Xperia Sola.
+#
+
+# ST-iNemo
+SOMC_CFG_SENSORS_COMPASS_LSM303DLHC := yes
+SOMC_CFG_SENSORS_ACCELEROMETER_LSM303DLHC_LT := yes
+
+# Proximity Sensor
+SOMC_CFG_SENSORS_PROXIMITY_APDS9702 := yes
+
+# configure light sensor
+SOMC_CFG_SENSORS_LIGHT_RANGE := 50000
